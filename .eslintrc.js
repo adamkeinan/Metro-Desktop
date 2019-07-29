fb4a47aa1b4685ad6a368e421a1c5e2d96efdfbd
adamkeinan <adamkeinan1@gmail.com>
2019-07-29 08:47:59 +0300

    added	new file:   .browserlistrc added	new file:   .eslintrc added	new file:   .eslintrc.js added	new file:   .eslintrc.json added	new file:   src/components/component.js added	new file:   styles/styles.scss added	new file:   webpack.parts.js second commit	modified:   .babelrc second commit	modified:   package.json second commit	modified:   src/index.js second commit	modified:   webpack.config.js

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "parser": "babel-eslint",
+        "rules": {
+            "max-len": [1, 120, 2, {ignoreComments: true}]
+        }
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
