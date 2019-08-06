103ad9f5c1ef9d0430fa4e3237d3f6617d49d935
adamkeinan <adamkeinan1@gmail.com>
2019-08-06 11:03:10 +0300

    initial commit	new file:   .editorconfig initial commit   .prettierrc initial commit   documentation/babel/babel-external-helpers initial commit   documentation/babel/babel-external-helpers.cmd initial commit   documentation/babel/babel-node initial commit   documentation/babel/babel-node.cmd initial commit   documentation/babel/babel.cmd initial commit   documentation/babel/babel.scrip.js initial commit   documentation/webpack/ihatetomatos.webpack.config.js initial commit   documentation/webpack/webpack execution process.png initial commit   documentation/webpack/webpack.config.backup.js initial commit   documentation/webpack/webpack.config.js initial commit   documentation/webpack/webpack.config.material_demo initial commit   documentation/webpack/webpack.config.p.js initial commit   documentation/webpack/webpack.config.skeleton initial commit   documentation/webpack/webpack.configv2.log initial commit   documentation/webpack/webpack.default.js initial commit   documentation/webpack/webpack.dev.config initial commit   documentation/webpack/webpack.dev.config.log initial commit   documentation/webpack/webpack.prod.js initial commit   documentation/webpack/webpack.prod.js.log initial commit   eslint-formatter-pretty/log.js initial commit   prettier after/webpack.config.js initial commit   webpack.config.lint.js modified   .babelrc modified   .eslintrc modified   .eslintrc.js modified   metroapp.js modified   package.json modified   server.js modified   src/index.js modified   src/routes/index.jsx modified   webpack.config.js modified   webpack.parts.js

---
+++ documentation/webpack/webpack.config.p.js
@@ -0,0 +1,32 @@
+import path;
+import HtmlWebpackPlugin;
+import ScriptExtHtmlWebpackPlugin;
+export default {
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js/,
+            exclude: /(node_modules|bower_components)/,
+            use: [{
+                loader: 'babel-loader'
+            }]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Custom template',
+            template: path.join(__dirname, 'src/index.template.html')
+        }),
+        new ScriptExtHtmlWebpackPlugin({
+            defaultAttribute: 'defer'
+        })
+    ],
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
