6c0ee13ef81076ce35319b259cb6142519e004b7
adamkeinan <adamkeinan1@gmail.com>
2019-08-22 10:52:31 +0300

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,110 +1,28 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-    commonjs: true,
-  },
-  extends: [
-    'airbnb',
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-    'prettier/flowtype',
-    'prettier/standard',
-    'prettier/react',
-  ],
-  settings: {
-    'react': {
-      'pragma': 'react',
-      'version': '16.9.0'
-    }
-  },
-  globals: {
-    window: true,
-    console: true,
-    document: true,
-    FormData: true,
-    FileReader: true,
-    Blob: true,
-    navigator: true,
-  },
-  'parser':'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-      },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'jsx-a11y',
-    'import',
-    'react',
-    'flowtype',
-    'prettier',
-  ],
-  rules: {  
-    'allowKeywords': [0, 'always'],
-    'allowPattern': [1, '^[a-zA-Z]+(_[a-zA-Z]+)+$'],
-    'array-bracket-spacing': [2, 'never'],
-    'code': 2,
-    'comments': 1,
-    'comma-dangle': [2, 'never'],
-    'consistent-return': 2,
-    'constructor-super': 'warn',
-    'flowtype/boolean-style': [2, 'boolean'],
-    'ident': [1, 4],
-    'ignoreComments': 2,
-    'ignorePattern': 2,
-    'ignoreRegExpLiterals': 2,
-    'ignoreStrings': 1,
-    'ignoreUrls': 1,
-    'ignoreTemplateLiterals': 1,
-    'ignoreTrailingComments': 1,
-    'import/imports-first': ['error', 'absolute-first'],
-    'import/newline-after-import': 'error',
-    'linebreak-style': ['off', 'eol-last'],
-    'max-len': [2, 2000 ],
-    'max-depth': ['warn', 10],
-    'no-await-in-loop': 'error',
-    'no-const-assign': 'warn',
-    'no-console': 0,
-    'no-const-assign': 'error',
-    'no-constant-condition': 'error',
-    'no-delete-var': 'error',
-    'no-eq-null': 'error',
-    'no-duplicate-case': 'error',
-    'no-duplicate-imports': 'error',
-    'no-empty-function': 'warn',
-    'no-else-return': 1,
-    'no-extra-semi': 'error',
-    'no-mixed-operators': 1,
-    'no-plusplus': 1,
-    'no-param-reassign': 1,
-    'no-restricted-syntax': 1,
-    'no-undef': 'warn',
-    'no-unreachable': 'warn',
-    'no-unused-vars': 0,
-    'no-this-before-super': 'warn',
-    'object-curly-spacing': [2, 'never'],
-    'prefer-destructuring': 0,
-    'prettier/prettier': ['error', {'singleQuote': true, 'parser': 'flow', 'usePrettierrc': true}],
-    'quotes': ['error', 'single'],
-    'react/jsx-filename-extensions': [1, { 
-      'extensions': ['*.js', '*.jsx'] }],
-    'react/jsx-uses-react': 'error',
-    'react/jsx-uses-vars': 'error',
-    'react/no-this-in-sfc': 0,
-    'react/destructuring-assignment': 0,
-    'react/jsx-one-expresion-per-line': 0,
-    'react/jsx-indent': 0,
-    'react/prop-types': 0,
-    'semi': [1, 'always'],
-    'space-unary-ops': 2,         
-    'tabwidth': 2,
-    'trailing-comma': 'off',
-    'global-require': 1,   
-    'valid-jsdoc': 1,
-    'valid-typeof': 'error'
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
     },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "parser": "babel-eslint",
+        "rules": {
+            "max-len": [1, 120, 2, {ignoreComments: true}]
+        }
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 };
